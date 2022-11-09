eb5493c5cd7be39ca67308f699de71b363180725
1Mathias <mathiasabdisa@gmail.com>
2022-11-09 07:55:18 +0300

    Home Page updates

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import Banner from './Banner'
-import  './Home.css'
+import './Home.css';
 
 function Home() {
   return (
     <div className='home'>
-        <Banner/>
+      <Banner />
+      <div className='home_section'>
+
+      </div>
+      {/*  
+        Header
+        Banner
+        Cards
+        Footer 
+        Search Page
+        */}
+
     </div>
   )
 }
