3b0b7de8bff9fffe240aa4e2b4d06f0556b827f1
1Mathias <mathiasabdisa@gmail.com>
2022-11-09 18:16:47 +0300

    Application updates

--- src/App.js
+++ src/App.js
@@ -3,14 +3,31 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from './SearchPage';
+import Footer from './Footer';
+
+
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Home/>
-      {/* Header */}
+    //BEM
+    <div className='app'>
+      <Router>
+        <Header />
 
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
     </div>
+
   );
 }
 
