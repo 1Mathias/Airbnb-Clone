fcdadb0bde030907fc8be4f17aa8bd3d56178fb3
1Mathias <mathiasabdisa@gmail.com>
2022-11-09 12:54:16 +0300

    Footer Updated

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>© 2022  <a href='https://1-mathias-github-io.vercel.app/' target="_blank">Mathias Abdissa</a> No rights reserved - this is a demo application!</p>
+
+        </div>
+    )
+}
+
+export default Footer
